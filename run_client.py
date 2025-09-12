9c159ed04e72841f7f0506f0547d743be575cb2a
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2025-09-12 07:12:43 -0400

    feat: added boundary check for coordinates and soil property classification

--- run_client.py
+++ run_client.py
@@ -2,9 +2,26 @@ import requests
 import os
 from dotenv import load_dotenv
 from soil_property import get_soil_property
-from soil_property_classification import get_soil_classification
+from soil_property_classification import (
+    get_nitrogen_classification,
+    get_phosphorous_classification,
+    get_potassium_classification,
+    get_ph_classification
+)
 
 load_dotenv()
+
+
+def check_coordinates(lattitude, longitude):
+    """
+    check if the coordinates entered are within Africa
+    return True if in Africa else False
+    """
+    if lattitude >= -35.0 and lattitude <= 37.0:
+        if longitude >= -18.0 and longitude <= 52:
+            return True
+    return False
+
 
 
 base_url = os.environ.get('BASE_URL')
@@ -23,16 +40,16 @@ if not isda_password:
     exit(1)
 
 
-depth_values = ['0-20', '0-50', '20-50', '0-200', '0']
+depth_values = ['0-20']
 
 lattitude = input('Enter lattitude of the location in Africa:    ').strip()
 longitude = input('Enter longitude of the location in Africa:    ').strip()
 depth = input('Enter the depth:    ').strip()
 
+
 if not (lattitude or longitude):
     print('\nError: Please enter the lattitude and longitude of the location')
     exit(1)
-
 
 try:
     lattitude = float(lattitude)
@@ -40,9 +57,14 @@ try:
 except ValueError:
     print('\nError: Lattitude and Longitude should be decimal values')
 
+is_within_africa = check_coordinates(lattitude, longitude)
+if not is_within_africa:
+    print('\nError: Co-ordinates are outside Africa. Please provide a location in Africa')
+    exit(1)
+
 
 if depth not in depth_values:
-    print('Error: Depth must be any value in (0-20, 0-50, 20-50, 0-200, 0)')
+    print('Error: Depth must be topsoil value (0-20cm)')
     exit(1)
 
 
@@ -55,6 +77,7 @@ if response.status_code != 200:
 else:
     response_data = response.json()
     access_token = response_data.get('access_token')
+    #print(access_token)
     
 
 soil_property = get_soil_property(lattitude, longitude, depth, access_token)
@@ -63,5 +86,16 @@ phosphorous_value = soil_property['property']['phosphorous_extractable'][0]['val
 potassium_value = soil_property['property']['potassium_extractable'][0]['value']['value']
 pH_value = soil_property['property']['ph'][0]['value']['value']
 
+soil_classification = dict()
+
+nitrogen_classification = get_nitrogen_classification(nitrogen_value)
+phosphorous_classification = get_phosphorous_classification(phosphorous_value)
+potassium_classification = get_potassium_classification(potassium_value)
+pH_classification = get_ph_classification(pH_value)
+
+print(nitrogen_classification)
+print(phosphorous_classification)
+print(potassium_classification)
+print(pH_classification)
 
 
--- soil_property_classification.py
+++ soil_property_classification.py
@@ -1,5 +1,60 @@
+"""
+Helper function to classify soil property (N,P,K, pH)
+values to either "LOW", "MODERATE" or "HIGH"
+based on given thresholds
+"""
+
+
+def get_nitrogen_classification(pH_value):
+    """
+    returns the classification of Nitrogen in location
+    """
+    pH_value = float(pH_value)
+    if pH_value is None or pH_value <= 1.5:
+        return "LOW"
+    elif pH_value > 1.5 and pH_value <= 5.0:
+        return "MODERATE"
+    else:
+        return "HIGH"
 
 
 
-def get_soil_classification():
-    pass
+def get_phosphorous_classification(phosphorous_value):
+    """
+    returns the classification of Phosphorous in location
+    """
+    phosphorous_value = float(phosphorous_value)
+    if phosphorous_value is None or phosphorous_value <= 10:
+        return "LOW"
+    elif phosphorous_value > 10 and phosphorous_value <= 50:
+        return "MODERATE"
+    else:
+        return "HIGH"
+
+
+
+def get_potassium_classification(potassium_value):
+    """
+    returns the classification of Potassium in location
+    """
+    potassium_value = float(potassium_value)
+    if potassium_value is None or potassium_value <= 39:
+        return "LOW"
+    elif potassium_value > 39 and potassium_value <= 195:
+        return "MODERATE"
+    else:
+        return "HIGH"
+
+
+
+def get_ph_classification(pH_value):
+    """
+    returns the classification of pH in location
+    """
+    pH_value = float(pH_value)
+    if pH_value is None or pH_value <= 5.3:
+        return "LOW"
+    elif pH_value > 5.3 and pH_value <= 7.3:
+        return "MODERATE"
+    else:
+        return "HIGH"
